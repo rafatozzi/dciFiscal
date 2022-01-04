17b39e55cef4497f65c503dc4d6f3666c80dce54
Rafael Tozzi <rafael.tozzi@hotmail.com>
2022-01-04 17:02:32 -0300

    Correção bug favoritos

--- src/modules/produtos/useCases/findFavoritos/FindFavoritosController.ts
+++ src/modules/produtos/useCases/findFavoritos/FindFavoritosController.ts
@@ -6,7 +6,7 @@ export class FindFavoritosController {
   async handle(request: Request, response: Response): Promise<Response> {
     const useCase = container.resolve(FindFavoritosUseCase);
 
-    const result = useCase.execute(request.cod_cliente);
+    const result = await useCase.execute(request.cod_cliente);
 
     return response.status(200).json(result);
   }
