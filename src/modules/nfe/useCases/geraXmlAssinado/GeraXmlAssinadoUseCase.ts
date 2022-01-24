1cd4a65df8188dca738103be6eca8044dd821109
Rafael Tozzi <rafael.tozzi@hotmail.com>
2022-01-24 17:37:09 -0300

    .

--- src/modules/nfe/useCases/geraXmlAssinado/GeraXmlAssinadoUseCase.ts
+++ src/modules/nfe/useCases/geraXmlAssinado/GeraXmlAssinadoUseCase.ts
@@ -133,13 +133,17 @@ export class GeraXmlAssinadoUseCase {
     const formData = new FormData();
 
     const certFolder = resolve(__dirname, "..", "..", "..", "..", "..", "archives", "cert");
-    const file = fs.readFileSync(`${certFolder}/${empresa.id}.pfx`, { encoding: 'base64' });
+    // const file = fs.readFileSync(`${certFolder}/${empresa.id}.pfx`, { encoding: 'base64' });
+    const file = fs.createReadStream(`${certFolder}/${empresa.id}.pfx`);
 
     formData.append("json", JSON.stringify(jsonRequest));
-    formData.append("certificado", file, "certificado.pfx");
+    formData.append("certificado", file, { knownLength: fs.statSync(`${certFolder}/${empresa.id}.pfx`).size });
 
     await axios.post(`${process.env.URL_NFE_PHP}/xml_assinado.php`, formData, {
-      headers: { ...formData.getHeaders() }
+      headers: {
+        ...formData.getHeaders(),
+        "Content-Length": formData.getLengthSync()
+      },
     })
       .then(async (res) => {
         if (!res.data || !res.data.xml || !res.data.chave) {
@@ -165,11 +169,10 @@ export class GeraXmlAssinadoUseCase {
         await nfeRepositories.create({ ...nfe, chave: res.data.chave, nr_nfe: nrNFe });
 
         await Queue.add("EnviaLote", { idNfe, cod_cliente });
-
       })
       .catch((err) => {
-        console.log(err.response.data);
-        throw new Error(err.response.data);
+        console.log(err.response);
+        throw new Error(err);
       });
   }
 }
