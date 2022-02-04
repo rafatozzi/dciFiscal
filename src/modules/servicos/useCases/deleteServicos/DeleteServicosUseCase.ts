456dfd96c87d2a2656dd6a943b46c17daf2179c3
Rafael Tozzi <rafael.tozzi@hotmail.com>
2022-02-04 15:10:26 -0300

    .

--- src/modules/servicos/useCases/deleteServicos/DeleteServicosUseCase.ts
+++ src/modules/servicos/useCases/deleteServicos/DeleteServicosUseCase.ts
@@ -1,5 +1,7 @@
 import { injectable } from "tsyringe";
 import { AppError } from "../../../../shared/errors/AppError";
+import { ServicosCkeckListRepositories } from "../../infra/typeorm/repositories/ServicosCkeckListRepositories";
+import { ServicosComissaoRepositories } from "../../infra/typeorm/repositories/ServicosComissaoRepositories";
 import { ServicosRepositories } from "../../infra/typeorm/repositories/ServicosRepositories";
 
 @injectable()
@@ -8,10 +10,24 @@ export class DeleteServicosUseCase {
 
   async execute(cod_cliente: string, id: string): Promise<void> {
     const repositories = new ServicosRepositories(cod_cliente);
+    const checklistRepositories = new ServicosCkeckListRepositories(cod_cliente);
+    const comissaoRepositories = new ServicosComissaoRepositories(cod_cliente);
+
     const servico = await repositories.findById(id);
 
     if (!servico)
       throw new AppError("Serviço não encontrado");
+
+    const checkLists = await checklistRepositories.findByServico(servico.id);
+    const comissoes = await comissaoRepositories.findByServico(servico.id);
+
+    checkLists.map(async (item) => {
+      await checklistRepositories.deleteById(item.id);
+    })
+
+    comissoes.map(async (item) => {
+      await comissaoRepositories.deleteById(item.id);
+    })
 
     await repositories.deleteServico(id);
   }
