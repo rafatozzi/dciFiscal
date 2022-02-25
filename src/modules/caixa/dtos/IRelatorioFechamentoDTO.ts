67a90a8d8c6642f0a2c3cfa7b1a7f59529d5225a
Rafael Tozzi <rafael.tozzi@hotmail.com>
2022-02-25 10:00:47 -0300

    .

--- src/modules/caixa/dtos/IRelatorioFechamentoDTO.ts
+++ src/modules/caixa/dtos/IRelatorioFechamentoDTO.ts
@@ -1,6 +1,8 @@
 export interface IRelatorioFechamentoDTO {
   valor_inicial: string;
   dinheiro: string;
+  cartao_credito: string;
+  cartao_debito: string;
   movDinheiro: string;
   movCartaoCredito: string;
   movCartaoDebito: string;
