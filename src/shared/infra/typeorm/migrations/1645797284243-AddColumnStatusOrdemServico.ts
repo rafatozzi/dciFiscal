5653f0bff7b7b14976fd4358228eaa7c4212a59b
Rafael Tozzi <rafael.tozzi@hotmail.com>
2022-02-25 11:06:27 -0300

    .

---
+++ src/shared/infra/typeorm/migrations/1645797284243-AddColumnStatusOrdemServico.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from "typeorm";
+
+export class AddColumnStatusOrdemServico1645797284243 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE ordem_servico ADD COLUMN `id_status` varchar(255) NOT NULL AFTER `id_user`");
+
+        await queryRunner.createForeignKey(
+            "ordem_servico",
+            new TableForeignKey({
+                name: "FKOrdemServicoStatus",
+                referencedTableName: "status",
+                referencedColumnNames: ["id"],
+                columnNames: ["id_status"],
+                onDelete: "CASCADE",
+                onUpdate: "CASCADE"
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey("ordem_servico", "FKOrdemServicoStatus");
+        await queryRunner.dropColumn("ordem_servico", "id_status");
+    }
+
+}
