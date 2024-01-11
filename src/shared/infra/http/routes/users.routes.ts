d1f59dd1884704b57a723949533cff06e029983a
Rafael Tozzi <rafael.tozzi@hotmail.com>
2024-01-11 14:26:49 -0300

    add resetPassword

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,17 +4,21 @@ import { EnsureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../../../../modules/accounts/useCases/createUser/CreateUserController";
 import { ProfileUserController } from "../../../../modules/accounts/useCases/profileUser/ProfileUserController";
 import { ListUsersController } from "../../../../modules/accounts/useCases/listUsers/ListUsersController";
+import { ResetPasswordController } from "../../../../modules/accounts/useCases/resetPassword/resetPasswordController";
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
 const profileUserController = new ProfileUserController();
 const listUsersController = new ListUsersController();
+const resetPasswordController = new ResetPasswordController();
 
 usersRouter.post("/", createUserController.handle);
 
 usersRouter.get("/", EnsureAuthenticated, profileUserController.handle);
 
 usersRouter.get("/list", EnsureAuthenticated, listUsersController.handle);
+
+usersRouter.get("/resetPassword", EnsureAuthenticated, resetPasswordController.handle);
 
 export { usersRouter };
